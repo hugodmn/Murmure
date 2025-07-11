a98d09b5ab2838f8750ace21ddf125a2a49fcf32
electron260 <hugo.daumain@edu.devinci.fr>
2025-07-11 18:41:59 +0200

    big_update

--- build/lib/murmure/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from .transcription.transcription import TranscriptionModule
-from .vad.vad import VADModule
-from .speaker_diarization.speaker_diarization import SpeakerDiarizationModule
-from .types import TranscriptionOutput
